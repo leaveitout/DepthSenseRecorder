fae90fc1cf91dbcb51678bb6496329f68f7e0333
Seán Bruton <sbruton@tcd.ie>
2015-02-18 14:33:54 +0000

    Eliminated linker errors due to circular dependencies.

--- src/DS325PlanarSegmentation.cpp
+++ src/DS325PlanarSegmentation.cpp
@@ -22,33 +22,31 @@ int main(int argc, char *argv[]) {
             camera->depthSize().width, camera->depthSize().height));
 
     while(!viewer->wasStopped(30)) {
-
-
 //        camera->captureDepth(depth);
 //        camera->captureAmplitude(amplitude);
 //        camera->captureColor(color);
 //        camera->captureColoredPointCloud(cloud);
         camera->capturePointCloud(cloud);
 
-//        pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients);
-//        pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
-//        // Create the segmentation object
-//        pcl::SACSegmentation<pcl::PointXYZ> seg;
-//        // Optional
-//        seg.setOptimizeCoefficients (true);
-//        // Mandatory
-//        seg.setModelType (pcl::SACMODEL_PLANE);
-//        seg.setMethodType (pcl::SAC_RANSAC);
-//        seg.setDistanceThreshold (0.01);
-//
-//        seg.setInputCloud (cloud);
-//        seg.segment (*inliers, *coefficients);
-//
-//        if (inliers->indices.size () == 0)
-//        {
-//            PCL_ERROR ("Could not estimate a planar model for the given dataset.");
-//            //return (-1);
-//        }
+        pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients);
+        pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
+        // Create the segmentation object
+        pcl::SACSegmentation<pcl::PointXYZ> seg;
+        // Optional
+        seg.setOptimizeCoefficients (true);
+        // Mandatory
+        seg.setModelType (pcl::SACMODEL_PLANE);
+        seg.setMethodType (pcl::SAC_RANSAC);
+        seg.setDistanceThreshold (0.01);
+
+        seg.setInputCloud (cloud);
+        seg.segment (*inliers, *coefficients);
+
+        if (inliers->indices.size () == 0)
+        {
+            PCL_ERROR ("Could not estimate a planar model for the given dataset.");
+            //return (-1);
+        }
 
 //        cv::Mat d, a;
 //        depth.convertTo(d, CV_8U, 255.0 / 1000.0);
